6b7c3bffe552a2d247f6c305d30c40afe22dbbfa
Dawn Miceli <dawnmiceli100@yahoo.com>
2015-03-17 15:35:03 -0400

    update config files for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => "dmm-myflix.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
